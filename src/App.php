88ce1711032264cc5f033a2becd95737d527230d
dotcoo <dotcoo@163.com>
2024-03-31 09:19:37 +0800

    添加 App

---
+++ src/App.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2021 The dotcoo <dotcoo@163.com>. All rights reserved.
+
+declare(strict_types=1);
+
+namespace zay;
+
+final class App {
+  public function start() {
+    return 'OK';
+  }
+}
